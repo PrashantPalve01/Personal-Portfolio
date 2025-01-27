92cac6814094099bc5174032eb0f36eac0dde973
PrashantPalve01 <147232012+PrashantPalve01@users.noreply.github.com>
2025-01-27 22:03:19 +0530

    update project section

--- src/components/Portfolio/project.js
+++ src/components/Portfolio/project.js
@@ -18,4 +18,12 @@ export const project = [
     git: "https://github.com/PrashantPalve01/B41_WEB_031_Frontend-Force",
     deploy: "https://ecommerce-reebok-website.netlify.app/",
   },
+  // {
+  //   img: movie,
+  //   title: "Movie Booking Website",
+  //   desc: "Developed a Movie Booking Website with features like user authentication, movie search, and booking. Focused on user experience by implementing Firebase authentication for secure data storage and enhancing performance and scalability.",
+  //   tech: "HTML, CSS, Tailwind CSS, JavaScript, Firebase",
+  //   git: "https://github.com/PrashantPalve01/Movie-Booking-System",
+  //   deploy: "https://movie-booking-solar-sparks.netlify.app/pages/home",
+  // },
 ];
