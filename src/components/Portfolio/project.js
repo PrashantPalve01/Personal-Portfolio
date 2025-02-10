281224ac0dead15607563b18c875c1c5d3b2dc39
PrashantPalve01 <147232012+PrashantPalve01@users.noreply.github.com>
2025-02-10 15:20:15 +0530

    New Project Added

--- src/components/Portfolio/project.js
+++ src/components/Portfolio/project.js
@@ -1,7 +1,16 @@
 import movie from "../../img/movie.png";
 import reebok from "../../img/reebok.png";
+import MediTrack from "../../img/MediTrack.png";
 
 export const project = [
+  {
+    img: MediTrack,
+    title: "MediTrack",
+    desc: "MediTrack is a smart medication management app that helps users track, manage, and stay consistent with their medication schedules. It features real-time reminders, adherence tracking, and an interactive calendar to ensure users never miss a dose.",
+    tech: "HTML, CSS, Tailwind CSS, React JS, JavaScript, Firebase",
+    git: "https://github.com/PrashantPalve01/Medication-Management-App",
+    deploy: "https://meditrackapplication.netlify.app/",
+  },
   {
     img: movie,
     title: "Movie Booking Website",
@@ -18,12 +27,4 @@ export const project = [
     git: "https://github.com/PrashantPalve01/B41_WEB_031_Frontend-Force",
     deploy: "https://ecommerce-reebok-website.netlify.app/",
   },
-  // {
-  //   img: movie,
-  //   title: "Movie Booking Website",
-  //   desc: "Developed a Movie Booking Website with features like user authentication, movie search, and booking. Focused on user experience by implementing Firebase authentication for secure data storage and enhancing performance and scalability.",
-  //   tech: "HTML, CSS, Tailwind CSS, JavaScript, Firebase",
-  //   git: "https://github.com/PrashantPalve01/Movie-Booking-System",
-  //   deploy: "https://movie-booking-solar-sparks.netlify.app/pages/home",
-  // },
 ];
