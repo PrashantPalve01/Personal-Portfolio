ec926eca69c6ac062ae0751a3ca4e2df8412cdf9
PrashantPalve01 <147232012+PrashantPalve01@users.noreply.github.com>
2025-03-05 18:31:22 +0530

    Added GitHub Recruiter Project

--- src/components/Portfolio/project.js
+++ src/components/Portfolio/project.js
@@ -1,8 +1,17 @@
 import movie from "../../img/movie.png";
 import reebok from "../../img/reebok.png";
 import MediTrack from "../../img/MediTrack.png";
+import hubHire from "../../img/hubHire.png";
 
 export const project = [
+  {
+    img: hubHire,
+    title: "GitHub Recruiter",
+    desc: "Developed a HubHire during a 48-hour hackathon, aimed at helping recruiters find developers based on their GitHub activity, projects, and skills. Implemented features like profile search, real-time filtering, and user recommendations.",
+    tech: "React.js, Chakra UI, Firebase, CSS, Node.js, Express.js, MongoDB",
+    git: "https://github.com/PrashantPalve01/gitmatch-hackathon",
+    deploy: "https://gitmatch-hackathon-frontend.onrender.com/",
+  },
   {
     img: MediTrack,
     title: "MediTrack",
